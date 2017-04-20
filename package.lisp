3d3cb084fcc3ca51609e45877479a0b83d0ad512
Nick Matvyeyev <mnasoft@gmail.com>
2017-04-20 07:28:01 +0300

    2017-4-20 7:27:59 MNASOFT-01

--- package.lisp
+++ package.lisp
@@ -5,7 +5,7 @@
   (:export η_sazerlend) ; Коэффициент динамической вязкости для некоторых газов в зависимости от абсолютной температуры по формуле Сазерленда
   (:export k)
   (:export μ)
-  (:export  Rμ)
+  (:export Rμ)
   )
 
 ;;;;(declaim (optimize (space 0) (compilation-speed 0)  (speed 0) (safety 3) (debug 3)))
